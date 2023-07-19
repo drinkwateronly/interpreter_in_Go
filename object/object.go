80038a5617900d75340fa1832588f04d7dfe4b45
Jie Chen <44942210+drinkwateronly@users.noreply.github.com>
2023-07-19 10:15:23 +0800

    finish ch3

--- object/object.go
+++ object/object.go
@@ -27,17 +27,21 @@ type Object interface {
 // Environment ##############################################
 type Environment struct {
 	store map[string]Object
+	outer *Environment
 }
 
 // 为什么不直接使⽤map，⽽是使⽤封装
 
 func NewEnvironment() *Environment {
 	s := make(map[string]Object)
-	return &Environment{store: s}
+	return &Environment{store: s, outer: nil}
 }
 
 func (e *Environment) Get(name string) (Object, bool) {
 	obj, ok := e.store[name]
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.Get(name)
+	}
 	return obj, ok
 }
 
@@ -112,3 +116,9 @@ func (f *Function) Inspect() string {
 }
 
 func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
+
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+	env := NewEnvironment()
+	env.outer = outer
+	return env
+}
